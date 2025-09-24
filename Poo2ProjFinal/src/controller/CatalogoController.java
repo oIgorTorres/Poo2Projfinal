3b881b4228a4a101629648f84b0f6f83894379a6
arthur <arthur.rodrigue64@gmail.com>
2025-09-24 20:45:37 -0300

    revisão 4

--- Poo2ProjFinal/src/controller/CatalogoController.java
+++ Poo2ProjFinal/src/controller/CatalogoController.java
@@ -4,6 +4,7 @@ package controller;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JOptionPane;
 import model.Catalogo;
